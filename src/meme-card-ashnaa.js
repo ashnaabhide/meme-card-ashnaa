a2cbe9f26fa714289c944137e130d11ae2a9ab21
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-02-16 13:45:24 -0500

    slots and meme

--- src/meme-card-ashnaa.js
+++ src/meme-card-ashnaa.js
@@ -119,17 +119,18 @@ details{
     
 <div class="wrapper">
   <div class="container">
-  <img class="image" src="https://static.boredpanda.com/blog/wp-content/uploads/2020/05/700-1.jpg" />
+  <meme-maker alt="borzoi meme" image-url="https://static.boredpanda.com/blog/wp-content/uploads/2020/05/700-1.jpg
+" top-text="BORZOI" bottom-text="woof. ">
+</meme-maker>
+  <!--<img class="image" src="https://static.boredpanda.com/blog/wp-content/uploads/2020/05/700-1.jpg" /> -->
   <div class="header">
   <h3>${this.name}</h3>
   <h4>${this.position}</h4>
     <details class="details">
       <summary>Details</summary>
       <div>
-        <ul>
-        <li>The magnificent borzoi! </li>
-        <li>Look how  disproportionate its nose is. Marvelous. </li>
-        </ul>
+        <slot name="paragraph">The magnificent borzoi! Look how  disproportionate its nose is. Marvelous. </slot>
+      
       </div>
   </details>
   </div>
